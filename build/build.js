45a56f77a00e598d4ea4a0f08483c5a2354b6c53
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2017-03-10 18:41:56 -0300

    Updates

--- build/build.js
+++ build/build.js
@@ -29207,12 +29207,6 @@ bespoke.from('article', [
       s.innerHTML = styleString;
       document.head.appendChild(s);
     },
-    elementStyles: function(slideEl, elementsAndTheirStyles) {
-      Object.keys(elementsAndTheirStyles).forEach(selector => {
-        Array.from(slideEl.querySelectorAll(selector))
-        .forEach(el => el.setAttribute('style', elementsAndTheirStyles[selector]));
-      });
-    },
     slideHash: function(slide, value) {
       slide.setAttribute('data-bespoke-hash', value);
     },
